6a20db4ce5b282b955d9fb0a2398ccbcce2e2080
rainmanp7 <muslimsoap@gmail.com>
2018-01-11 12:08:50 +0800

    p2p

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Copyright (c) 2014-2017 XDN-project developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x4a, 0x6c, 0x15, 0x6c, 0x6f, 0x20, 0x14, 0xd5, 0x2f, 0x6a, 0x79, 0x73, 0x7c, 0x6d, 0x2a, 0x20 } };
+}
