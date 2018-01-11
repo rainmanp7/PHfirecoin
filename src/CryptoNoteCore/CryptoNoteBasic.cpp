c587e83c38f93f0c5fde9c8fe31a6e3deb85b393
rainmanp7 <muslimsoap@gmail.com>
2018-01-11 12:13:12 +0800

    core areas to upload

---
+++ src/CryptoNoteCore/CryptoNoteBasic.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Copyright (c) 2014-2017 XDN-project developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "CryptoNoteBasic.h"
+#include "crypto/crypto.h"
+
+namespace CryptoNote {
+
+KeyPair generateKeyPair() {
+  KeyPair k;
+  Crypto::generate_keys(k.publicKey, k.secretKey);
+  return k;
+}
+
+}
---
+++ src/CryptoNoteCore/SwappedVector.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Copyright (c) 2014-2017 XDN-project developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include "SwappedVector.h"
+
+namespace {
+char suppressMSVCWarningLNK4221;
+}
