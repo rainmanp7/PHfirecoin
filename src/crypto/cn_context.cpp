5d952779c17581c4c5407aa1b3e5deca38bbbc1a
rainmanp7 <muslimsoap@gmail.com>
2018-01-21 13:12:21 +0800

    added

---
+++ src/crypto/cn_context.cpp
@@ -0,0 +1,5 @@
+//
+// Created by rainmanp7 on 1/21/18.
+//
+
+#include "cn_context.h"
---
+++ src/crypto/cn_context.h
@@ -0,0 +1,14 @@
+//
+// Created by rainmanp7 on 1/21/18.
+//
+
+#ifndef DIGITALNOTE_CN_CONTEXT_H
+#define DIGITALNOTE_CN_CONTEXT_H
+
+
+class cn_context {
+
+};
+
+
+#endif //DIGITALNOTE_CN_CONTEXT_H
